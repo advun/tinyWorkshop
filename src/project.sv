0585b4f83564fe3fc6c915fa7c212a75dde54bc4
advun <cameronpwilding@gmail.com>
2026-04-22 18:30:18 -0400

    first fixes

--- src/project.sv
+++ src/project.sv
@@ -5,6 +5,12 @@
 
 `default_nettype none
 
+//ui_in is input 8 bits
+//uo_out is data of packets
+//uio_in[0] hold high to operate
+//uio_out[2:1] holds packet code
+//uio_out[3] if high, save data
+
 module tt_um_advun (
     input  wire [7:0] ui_in,    // Dedicated inputs (DATA IN!!!)
     output wire [7:0] uo_out,   // Dedicated outputs
@@ -21,22 +27,24 @@ module tt_um_advun (
     localparam RAWTHRESHOLD = 8; //how big does delta have to be to go to raw? both pos and neg
     
     //packet codes
-    localparam RLE = 2'b00; //Normal run length encoding 
-    localparam DELTARLE = 2'b01; //run length encoding of deltas
-    localparam DELTA = 2'b10; //small delta change
-    localparam RAW = 2'b11; //Raw data bytes
+    localparam RLE = 2'b00; //Normal run length encoding.  2 bit packet code, 8 bit length of run
+    localparam DELTARLE = 2'b01; //run length encoding of deltas 2 bit packet code, 8 bit length of run
+    localparam DELTA = 2'b10; //small delta change. 2 bit packet code, 1 sign bit 2 bits of delta magnitude
+    localparam RAW = 2'b11; //Raw data byte. 2 bit packet code, 8 bits data
     
     //in/out assigns
-    assign uio_oe[0]= 0; //input 
-    assign uio_oe[1]= 1; //output
-    assign uio_oe[2]= 1; //output
-    assign uio_oe[3]= 1; //output
+    assign uio_oe[0]= 0; //input, start
+    assign uio_oe[1]= 1; //output, packet[0]
+    assign uio_oe[2]= 1; //output, packet[1]
+    assign uio_oe[3]= 1; //output, save
+    assign uio_oe[4]= 0; //input, NOT USED
+    assign uio_oe[5]= 0; //input, NOT USED
+    assign uio_oe[6]= 0; //input, NOT USED
+    assign uio_oe[7]= 0; //input, NOT USED
     
-    wire [7:0] in;
-    assign ui_in = in;
-    
-    wire start = uio_in[0];
-    
+    assign uio_out[0] = 0; //NOT USED
+    assign uio_out[7:4] = 0; //NOT USED
+        
     reg [1:0] packet;
     assign uio_out [2:1] = packet;
     
@@ -51,8 +59,8 @@ module tt_um_advun (
             4'b1100: out_r = RLE_count; //RLE Output w/outflag
             4'b0101: out_r = RLE_count; //DeltaRLE Output
             4'b1101: out_r = RLE_count; //DeltaRLE Output w/outflag
-            4'b0110: out_r = {largeDeltanew[2:0],5'b00000}; //delta
-            4'b1110: out_r = {largeDeltanew[2:0],5'b00000}; //delta  w/outflag
+            4'b0110: out_r = {largeDeltanew[DATA_WIDTH], largeDeltanew[2:0],5'b00000}; //delta
+            4'b1110: out_r = {largeDeltanew[DATA_WIDTH], largeDeltanew[2:0],5'b00000}; //delta  w/outflag
             4'b0111: out_r = storagenew; //raw
             4'b1111: out_r = storageold; //raw w/outflag
             default: out_r = 0;
@@ -64,7 +72,7 @@ module tt_um_advun (
     reg [DATA_WIDTH-1:0] storagenew; //store full values for delta encoding. new value
     reg [RLEWIDTH-1:0] RLE_count; //how many values/deltas same in a row
     //reg [RLEWIDTH-1:0] deltaRLE_count; //how many deltas same in a row (DONT NEED, JUST USE RLE_count)
-    reg [DATA_WIDTH-1:0] largeDeltaold; //last delta.  just change out for third storage??
+    reg signed [DATA_WIDTH:0] largeDeltaold; //last delta.
     reg signed [DATA_WIDTH:0] largeDeltanew; //current delta, 1 bit larger for sign bit
     reg OUTFLAG; //goes high if the previous output was skipped for rle or deltarle ending
     reg RLEFLAG; //low if RLE, high if DELTARLE
@@ -74,34 +82,39 @@ module tt_um_advun (
             storageold <= 0;
             storagenew <= STARTER; //initialize starting value at 0
             RLE_count <= 0; //reset run length counts
-            //deltaRLE_count <= 0;
             largeDeltaold <= 0;
             largeDeltanew <= 0;
             OUTFLAG <= 0;
+            RLEFLAG <= 0;
         end
         
         else begin
-             if (start) begin
+             if (uio_in[0]) begin
                  storageold <= storagenew; //move new values to old 
                  storagenew <= ui_in; //read in new values
+                 save <= 0;
     
                  if (OUTFLAG) begin
                     packet <= RAW;
+                    save <= 1; //save
                  end
                     
-                    if (storagenew == storageold) begin //check if same
-                        RLE_count <= RLE_count + 1; //RLE mode!
-                        OUTFLAG <= 0;
-                        RLEFLAG <= 0;
+                 if (storagenew == storageold) begin //check if same
+                     RLE_count <= RLE_count + 1; //RLE mode!
+                     OUTFLAG <= 0;
+                     RLEFLAG <= 0;
                         
                         if (RLE_count >= ((1 << RLEWIDTH) - 1)) begin //hit max value (avoid overflow)
                             packet <= RLE;
                             RLE_count <= 0;
+                            save <= 1;
                         end
-                    else begin //if different
+                 end
+                 else begin //if different
                         //RLE fail
                         if ((RLE_count > 0)&&(RLEFLAG == 0)) begin //if there is an RLE run, end it
                             packet <= RLE;
+                            save <= 1;
                             OUTFLAG <= 1;
                             RLE_count <= 0;
                         end
@@ -109,7 +122,7 @@ module tt_um_advun (
                         largeDeltaold <= largeDeltanew[DATA_WIDTH-1:0]; //update largeDeltaold
                         largeDeltanew <= storagenew - storageold;  //find delta
                             
-                        if (largeDeltanew[DATA_WIDTH-1:0] == largeDeltaold) begin //Delta RLE Mode
+                        if (largeDeltanew == largeDeltaold) begin //Delta RLE Mode
                             RLE_count <= RLE_count + 1; //increment counter by 1
                             OUTFLAG <= 0;
                             RLEFLAG <= 1;
@@ -117,6 +130,7 @@ module tt_um_advun (
                             if (RLE_count >= ((1 << RLEWIDTH) - 1)) begin //hit max value (avoid overflow)
                                 packet <= DELTARLE;
                                 RLE_count <= 0;
+                                save <= 1; 
                             end
                         end
                             
@@ -125,10 +139,12 @@ module tt_um_advun (
                                 packet <= DELTARLE;
                                 RLE_count <= 0;
                                 OUTFLAG <= 1;
+                                save <= 1;
                             end
                             
-                            else if ((largeDeltanew < RAWTHRESHOLD)&& (!OUTFLAG)) begin // if small delta: DELTA mode!
+                            else if ((largeDeltanew < RAWTHRESHOLD)&& (largeDeltanew > -RAWTHRESHOLD) && (!OUTFLAG)) begin // if small delta: DELTA mode!
                                     packet <= DELTA;
+                                    save <= 1;
                             end
                             
                             else begin //large delta: RAW mode!
@@ -137,6 +153,7 @@ module tt_um_advun (
                                  end
                                  else begin
                                     packet <= RAW;
+                                    save <= 1;
                                  end
                             end
                         end   
@@ -144,7 +161,6 @@ module tt_um_advun (
                 end
             end
         end
-    end
 
         
 endmodule
